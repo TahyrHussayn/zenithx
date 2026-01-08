ac15c49df8fa812522e0551d473ba1b0fbc94fc5
TahirHusayn <tahirhussain5112@gmail.com>
2026-01-08 23:50:41 +0530

    Project linked to vercel and vercel envs are synced with the local project also did setup vercel blob to access images more effeciently.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,14 @@
-export default function Home() { }
+import { list } from "@vercel/blob";
+import Image from "next/image";
+
+export default async function Home() {
+    const response = await list();
+
+    return (
+        <>
+            {response.blobs.map((blob) => (
+                <Image key={blob.pathname} src={blob.downloadUrl} alt='' width={100} height={100} />
+            ))}
+        </>
+    );
+}
